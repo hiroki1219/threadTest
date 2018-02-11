67d22e92bb18ac0ce8eed090a2e33249a372cb9e
池田弘樹 <ikedahiroki@ikedahirokinoMacBook-puro.local>
2018-02-11 18:33:54 +0900

    make project

---
+++ source/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+void input( int &check )
+{
+    while( true ){
+				    std::cin >> check;
+				}
+}
+
+void output( int &i, int &check )
+{
+
+    std::thread t2( input, std::ref(check) );
+				while( check != 1 ){
+								i++;
+								std::cout << i << "\n" << std::endl;
+								sleep( 1 );
+				}
+				t2.detach();
+}
+
+int main( void )
+{
+
+				int i = 0;
+				int check = 0;
+				std::thread t1( output, std::ref(i), std::ref(check) );
+				t1.join();
+
+				std::cout << "Last number : " << i << std::endl;
+
+				return 0;
+}
